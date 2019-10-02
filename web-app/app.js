eb7d8317541ba9dec2c10f4a35eda0d0a84fc956
oskar-g <oscarg@programmer.net>
2019-10-02 20:15:09 +0200

    mas ejemplos

--- web-app/app.js
+++ web-app/app.js
@@ -22,20 +22,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/demo', (req, res) => {
+    res.status(200)
+        .type('text/xml')
+        .end('<html><body><h1>ASDLKASLDKJSAD</h1></body></html>');
+});
+
+app.get('/saluda/:name', (req, res) => {
+    const { name } = req.params
+    res.status(200)
+        .type('text/plain')
+        .end(`Povale ${name}`);
+});
+
+app.get('/despide', (req, res) => {
+    res.status(200)
+        .type('text/plain')
+        .end('Talue');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
